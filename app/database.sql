a3bff7eb041e068f7ad92d7ad888c4336eb2e402
Leonardo Amaral <theleoamaral@gmail.com>
2024-09-23 14:07:40 -0300

    Refactor (Database): atualização da estrutura
    - Refatorada a estrutura da base de dados para utilizar foreign keys de forma mais apropriada.
    - Adicionada uma nova tabela para gerenciar os patterns utilizados na leitura dos logs.
    - Garantida a integridade referencial entre tabelas, melhorando a organização e a eficiência do banco de dados.
    - Atualizados os modelos e as relações para refletir as novas estruturas e garantir a compatibilidade com as operações de leitura.

--- app/database.sql
+++ app/database.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IF NOT EXISTS log_files (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    log_date DATETIME NOT NULL,  -- Data do nome do arquivo
-    log_type TEXT NOT NULL,       -- Tipo de log
-    file_name TEXT NOT NULL,      -- Nome completo do arquivo
-    file_path TEXT NOT NULL,      -- Caminho completo do arquivo
-    last_modified DATETIME NOT NULL,  -- Última modificação
-    creation_time DATETIME NOT NULL,   -- Criação no sistema
-    file_size INTEGER NOT NULL,        -- Tamanho em bytes
-    cursor_position INTEGER DEFAULT 0,  -- Posição do cursor
-    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime'))  -- Criação na DB
+    log_date DATETIME NOT NULL,
+    log_type TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    file_path TEXT NOT NULL,
+    last_modified DATETIME NOT NULL,
+    creation_time DATETIME NOT NULL,
+    file_size INTEGER NOT NULL,
+    cursor_position INTEGER DEFAULT 0,
+    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime'))
 );
 
 CREATE TABLE IF NOT EXISTS patterns (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     pattern TEXT NOT NULL,
-    updated_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),  -- Atualização
-    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime'))   -- Criação
+    updated_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),
+    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime'))
 );
 
 CREATE TABLE IF NOT EXISTS log_files_patterns (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     pattern_id INTEGER NOT NULL,
     log_file_id INTEGER NOT NULL,
-    updated_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),  -- Atualização
-    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),  -- Criação
+    updated_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),
+    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (pattern_id) REFERENCES patterns (id),
     FOREIGN KEY (log_file_id) REFERENCES log_files (id)
 );
@@ -32,10 +32,10 @@ CREATE TABLE IF NOT EXISTS logs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     pattern_id INTEGER NOT NULL,
     log_file_id INTEGER NOT NULL,
-    log_file_type TEXT NOT NULL,      -- Tipo do arquivo de log
-    log_date DATETIME NOT NULL,       -- Data estampada na linha do log
-    json_data TEXT NOT NULL,          -- Dados do log processados em JSON
-    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),  -- Criação na DB
+    log_file_type TEXT NOT NULL,
+    log_date DATETIME NOT NULL,
+    json_data TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT (datetime('now', 'localtime')),
     FOREIGN KEY (pattern_id) REFERENCES patterns (id),
     FOREIGN KEY (log_file_id) REFERENCES log_files (id)
 );
